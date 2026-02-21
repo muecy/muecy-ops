f956c6b0287773fed3093ba3250eaea5b7506e92
muecy <muecy25@gmail.com>
2026-02-21 18:35:41 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -101,6 +101,4 @@ app.listen(port, '0.0.0.0', () => {
   console.log(`Muëcy Ops running on port ${port}`);
   console.log(`Connect Google: ${process.env.APP_BASE_URL || "(set APP_BASE_URL)"}/auth/google`);
 });
-import { startBot } from "./bot.js";
-
 startBot({ userId: 1 });
